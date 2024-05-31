7723ece612968eaeb7024942ea59341b80901afa
yoga4dogs <witchkraftmacncheese@gmail.com>
2024-05-31 12:18:06 -0400

    first commit

---
+++ pet.py
@@ -0,0 +1,20 @@
+import random
+import tkinter as tk
+
+impath = '.\\images\\'
+impath += 'stupid_cat.png'
+x = 500
+
+window = tk.Tk()
+window.config(highlightbackground='black')
+window.overrideredirect(True)
+window.wm_attributes('-transparentcolor','black')
+
+idle = [tk.PhotoImage(file=impath,format = 'png')]
+
+label = tk.Label(window,bd=0,bg='black')
+label.configure(image=idle)
+label.pack()
+
+window.geometry('128x128+'+str(x)+'+922')
+window.mainloop()
