fb538fb50568033c1db5f8c4ea30f316ce8cc34c
yoga4dogs <witchkraftmacncheese@gmail.com>
2024-05-31 18:42:26 -0400

    somehin

--- pet.py
+++ pet.py
@@ -3,12 +3,15 @@ import tkinter as tk
 
 impath = '.\\images\\'
 impath += 'stupid_cat.png'
+
 x = 500
+y = 1080-256
 
 window = tk.Tk()
 window.config(highlightbackground='black')
 window.overrideredirect(True)
 window.wm_attributes('-transparentcolor','black')
+window.wm_attributes('-topmost', True)
 
 idle = [tk.PhotoImage(file=impath,format = 'png')]
 
@@ -16,5 +19,5 @@ label = tk.Label(window,bd=0,bg='black')
 label.configure(image=idle)
 label.pack()
 
-window.geometry('128x128+'+str(x)+'+922')
+window.geometry('128x128+'+str(x)+'+'+str(y))
 window.mainloop()
