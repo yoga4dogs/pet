96d14579dae1b178b330b7875d43d047bedffe6c
yoga4dogs <witchkraftmacncheese@gmail.com>
2024-05-31 23:06:14 -0400

    states

--- pet.py
+++ pet.py
@@ -3,58 +3,118 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import ctypes
 
+image_path = '.\\images\\'
+sprite_size = 128
+
 user32 = ctypes.windll.user32
 screen_size = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
 taskbar_size = 48
+bottom_bounds = screen_size[1]-sprite_size-taskbar_size
 
-image_path = '.\\images\\'
-sprite_size = 128
-facing = 1
+class new_yellowman:
+    def __init__(self):
+        self.x = random.randrange(0, screen_size[0]-sprite_size)
+    state = 'walk'
+    facing = 1
+    speed = 20
+    y = bottom_bounds
 
-speed = 20
+global yellowman 
+yellowman = new_yellowman()
+
 count = 0
 
-x = random.randrange(0, screen_size[0]-sprite_size)
-y = screen_size[1]-sprite_size-taskbar_size
-
-window = tk.Tk()
-window.config(highlightbackground='black')
-window.overrideredirect(True)
-window.wm_attributes('-transparentcolor','black')
-window.wm_attributes('-topmost', True)
+root = tk.Tk()
+root.config(highlightbackground='black')
+root.overrideredirect(True)
+root.wm_attributes('-transparentcolor','black')
+root.wm_attributes('-topmost', True)
 
 #call buddy's action .gif to an array
-walk = []
-walk.append(tk.PhotoImage(file=image_path+'yellowman\\walk.gif',format = 'gif -index 0'))
-walk.append(tk.PhotoImage(file=image_path+'yellowman\\walk.gif',format = 'gif -index 1'))
-walk.append(tk.PhotoImage(file=image_path+'yellowman\\walk.gif',format = 'gif -index 2'))
-walk.append(tk.PhotoImage(file=image_path+'yellowman\\walk.gif',format = 'gif -index 3'))
+walkL = []
+walkL.append(tk.PhotoImage(file=image_path+'yellowman\\walkL.gif',format = 'gif -index 0'))
+walkL.append(tk.PhotoImage(file=image_path+'yellowman\\walkL.gif',format = 'gif -index 1'))
+walkL.append(tk.PhotoImage(file=image_path+'yellowman\\walkL.gif',format = 'gif -index 2'))
+walkL.append(tk.PhotoImage(file=image_path+'yellowman\\walkL.gif',format = 'gif -index 3'))
+walkR = []
+walkR.append(tk.PhotoImage(file=image_path+'yellowman\\walkR.gif',format = 'gif -index 0'))
+walkR.append(tk.PhotoImage(file=image_path+'yellowman\\walkR.gif',format = 'gif -index 1'))
+walkR.append(tk.PhotoImage(file=image_path+'yellowman\\walkR.gif',format = 'gif -index 2'))
+walkR.append(tk.PhotoImage(file=image_path+'yellowman\\walkR.gif',format = 'gif -index 3'))
+fallL = []
+fallL.append(tk.PhotoImage(file=image_path+'yellowman\\fallL.gif',format = 'gif -index 0'))
+fallL.append(tk.PhotoImage(file=image_path+'yellowman\\fallL.gif',format = 'gif -index 1'))
+fallL.append(tk.PhotoImage(file=image_path+'yellowman\\fallL.gif',format = 'gif -index 2'))
+fallL.append(tk.PhotoImage(file=image_path+'yellowman\\fallL.gif',format = 'gif -index 3'))
+fallR = []
+fallR.append(tk.PhotoImage(file=image_path+'yellowman\\fallR.gif',format = 'gif -index 0'))
+fallR.append(tk.PhotoImage(file=image_path+'yellowman\\fallR.gif',format = 'gif -index 1'))
+fallR.append(tk.PhotoImage(file=image_path+'yellowman\\fallR.gif',format = 'gif -index 2'))
+fallR.append(tk.PhotoImage(file=image_path+'yellowman\\fallR.gif',format = 'gif -index 3'))
 
-def update(count, x, facing):
-    walk_frame = walk[count]
+def update(count, yellowman):
     if count < 3:
         count += 1
     else:
         count = 0
+    if yellowman.state == 'walk':
+        if yellowman.facing > 0:
+            frame = walkR[count]
+        else:
+            frame = walkL[count]
+            
 
-    if x < screen_size[0]-sprite_size and facing == 1:
-        x += speed
-    elif x > screen_size[0]-sprite_size and facing == 1:
-        facing *= -1
-    elif x > 0 and facing == -1:
-        x -= speed
-    elif x < 0:
-        facing *= -1
+        if yellowman.x + (yellowman.speed * yellowman.facing) > screen_size[0]-sprite_size:
+            yellowman.x = screen_size[0]-sprite_size
+            yellowman.facing *= -1
+        elif yellowman.x + (yellowman.speed * yellowman.facing) < 0:
+            yellowman.x = 0
+            yellowman.facing *= -1
+        else:
+            yellowman.x += yellowman.speed * yellowman.facing
 
-    window.geometry('128x128+'+str(x)+'+'+str(y))
-        
-    label.configure(image=walk_frame)
+    elif yellowman.state == 'held':
+        if yellowman.facing > 0:
+            frame = fallR[count]
+        else:
+            frame = fallL[count]
+        pass
+    elif yellowman.state == 'fall':
+        if yellowman.facing > 0:
+            frame = fallR[count]
+        else:
+            frame = fallL[count]
+        if yellowman.y + yellowman.speed * 2 > bottom_bounds:
+            yellowman.y = bottom_bounds
+            yellowman.state = 'walk'
+        else:
+            yellowman.y += yellowman.speed * 2
 
-    window.after(125,update, count, x, facing)
+    label.configure(image=frame)
+    root.geometry('128x128+'+str(yellowman.x)+'+'+str(yellowman.y))
+    root.after(125,update, count, yellowman)
 
-label = tk.Label(window,bd=0,bg='black')
-label.configure(image=walk[0])
+def set_fall(event):
+    yellowman.state = 'fall'
+
+def drag_handler(event):
+    yellowman.state = 'held'
+    if yellowman.x < int(root.winfo_pointerx()-sprite_size/2):
+        yellowman.facing = 1
+    else:
+        yellowman.facing = -1
+    global prev_x
+    prev_x = int(root.winfo_pointerx()-sprite_size/2)
+    yellowman.x = int(root.winfo_pointerx()-sprite_size/2)
+    yellowman.y = int(root.winfo_pointery()-sprite_size/4)
+
+
+label = tk.Label(root,bd=0,bg='black')
+label.configure(image=walkR[0])
 label.pack()
-window.geometry('128x128+'+str(x)+'+'+str(y))
-window.after(1,update, count, x, facing)
-window.mainloop()
+
+label.bind("<B1-Motion>", drag_handler)
+label.bind("<ButtonRelease>", set_fall)
+root.geometry('128x128+'+str(yellowman.x)+'+'+str(yellowman.y))
+root.after(1,update, count, yellowman)
+root.mainloop()
